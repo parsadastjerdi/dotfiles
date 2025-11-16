a4e81b018c21ce3b9a4e4767de0f542879524056
Parsa Dastjerdi <parsadastjerdi@gmail.com>
2025-11-15 22:57:57 -0800

    Move create-symlinks.sh to scripts folder

--- scripts/create-symlinks.sh
+++ scripts/create-symlinks.sh
@@ -3,10 +3,10 @@
 # default target is $HOME
 stow bash --target=$HOME
 stow helix --target="$HOME/.config/helix"
-stow idea --target=$HOME
+# stow idea --target=$HOME
 stow stow --target=$HOME
 stow tmux --target=$HOME
-stow vim --target=$HOME
+# stow nvim --target=$HOME
 # stow zsh/.zshrc --target=$HOME
 # stow zsh/.zshenv --target=$HOME
 stow zsh --target=$HOME
