91752b03d327ab2716c6cbc081bc599d03088ff5
Parsa Dastjerdi <parsadastjerdi@gmail.com>
2026-02-27 17:08:23 -0800

    Add yum repos

---
+++ yum/yum.repos.d
@@ -0,0 +1,7 @@
+sudo tee /etc/yum.repos.d/antigravity.repo << EOL
+[antigravity-rpm]
+name=Antigravity RPM Repository
+baseurl=https://us-central1-yum.pkg.dev/projects/antigravity-auto-updater-dev/antigravity-rpm
+enabled=1
+gpgcheck=0
+EOL
