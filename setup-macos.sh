3fc25442d1da934d97b4643277c13d0ee74dec72
Parsa Dastjerdi <parsadastjerdi@gmail.com>
2026-07-05 23:07:33 -0700

    Add Brewfile and init setup-macos.sh script

---
+++ setup-macos.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if ! command -v brew &> /dev/null; then
+    echo "Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo "Homebrew already installed"
+fi
+
+brew update
+brew bundle install
