49b52a3c57d85c16fc42c0a2d163978c5cbfbf7c
Parsa Dastjerdi <parsadastjerdi@gmail.com>
2026-07-06 21:15:27 -0700

    feat: Add symlinks to setup-macos.sh

--- setup-macos.sh
+++ setup-macos.sh
@@ -8,4 +8,22 @@ else
 fi
 
 brew update
+brew upgrade
 brew bundle install
+
+echo "Symlinking dotfiles..."
+cd "$(dirname "$0")"
+
+stow zsh --target=$HOME 2>/dev/null
+stow tmux --target=$HOME 2>/dev/null
+stow vim --target=$HOME 2>/dev/null
+stow stow --target=$HOME 2>/dev/null
+stow git --target=$HOME 2>/dev/null
+
+mkdir -p ~/.config/helix && stow helix --target=$HOME/.config/helix 2>/dev/null
+mkdir -p ~/.config/starship && stow starship --target=$HOME/.config/starship 2>/dev/null
+mkdir -p ~/.config/zed && stow zed --target=$HOME/.config/zed 2>/dev/null
+mkdir -p ~/.config/ghostty && stow ghostty --target=$HOME/.config/zed 2>/dev/null
+
+echo "Dotfiles symlinked"
+echo "Bootstrap complete"
